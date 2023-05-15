0c45dee0e135b18343ec3695c71c4795e9a37319
SHAIK-MOMIN-SARDAR-BASHA <111995451+SHAIK-MOMIN-SARDAR-BASHA@users.noreply.github.com>
2023-05-15 14:27:25 +0530

    condition_sts

---
+++ condition_sts.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n name : Sardar";
+	cout<<"\n fname: basha";
+	cout<<"\n doorno: 2/453";
+	cout<<"\n City: kadapa";
+	cout<<"\n Pncode: 5161385";
+	
+return 0;
+}
