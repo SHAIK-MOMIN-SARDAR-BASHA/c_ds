ef1129fba0299bd08fcbf2f78b9b01bc8dca0481
SHAIK-MOMIN-SARDAR-BASHA <111995451+SHAIK-MOMIN-SARDAR-BASHA@users.noreply.github.com>
2023-06-08 11:33:26 +0530

    Add files via upload

---
+++ total_avg marks.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m,p,c,total;
+	float avg;
+	cout<"/n enter three subjects marks";
+	cin>>m;
+	cin>>p;
+	cin>>c;
+	total=m+p+c;
+	cout<<"total marks is"<<total;
+	avg=total/3.0;
+    cout<<"Average Marks is"<<avg;
+    return 0;
+	
+	
+}
