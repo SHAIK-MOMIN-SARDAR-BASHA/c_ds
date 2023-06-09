780fa872c9d3b6cd68e99ebca41496c1d028c7ba
SHAIK-MOMIN-SARDAR-BASHA <111995451+SHAIK-MOMIN-SARDAR-BASHA@users.noreply.github.com>
2023-06-09 10:08:16 +0530

    Add files via upload

---
+++ break small.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int mounth_number
+	cout<<"\n Enter the month number";
+	cin>>month_number;
+	switch (month_number)
+	{
+		case 1:
+			cout<<"march";
+			break;
+		case 2:
+			cout<<"april";
+			break;
+		case 3:
+			cout<<"june";
+			break;
+		default:
+			cout<<"\nI nvalid Month";
+		
+		
+	}
+return 0;
+}
+	
+	
